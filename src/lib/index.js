5229818655d38a5c9d07c5f6f342984fc679b5bc
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-07-09 17:03:48 -0700

    init

---
+++ src/lib/index.js
@@ -0,0 +1,53 @@
+/**
+ * Creates a global object that allows replaying of failed logic at a deferred time.
+ * @param  {Function} fn     The function that may fail
+ * @return {Function}        A replica of the function that will internally catch errors and allow them to be executed sequentially at a deferred time.
+ */
+export default function tryDefer (condition = () => true) {
+  let _queue: []
+  let enqueue = (fn, args, errors) => _queue.push({ fn, args, errors })
+
+  function _flush () {
+    const queue = _queue
+    _queue = []
+    return queue
+  }
+
+  function replay (queue = _flush()) {
+    while(queue.length > 0) {
+      attempt(queue.shift())
+    }
+  }
+
+  function attempt ({ fn, args, errors = [], attempt = 0 }) {
+    try {
+      if(condition())
+        return fn(...args)
+      enqueue(fn, args, errors, attempt + 1)
+      return { deferred: true, attempt }
+    } catch(err) {
+      enqueue(fn, args, [ ...errors, err ], attempt + 1)
+      return { deferred: false, err, attempt }
+    }
+  }
+
+  function serialize() {
+    const serialized = require('serialize-javascript')({ execute: () => replay(_queue) })
+    return `
+if(typeof window === 'object') {
+  window.__replay__ = ${serialized};
+  window.__replay__.execute();
+}`
+  }
+
+  function reactReplay(React) {
+    return props => <script dangerouslySetInnerHTML={{ __html: serialize() }} />
+  }
+
+  const defer = { replay, serialize, reactReplay }
+
+  /** Returns a 2 item array of [ thunk, defer ] that wraps a function and functions for replaying in various scenarios. */
+  return ([ fn => (...args) => attempt({ fn, args })
+          , defer
+          ])
+}
